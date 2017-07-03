9f47c1b7e1b7bf682e9637810abab4d750890351
rafahurtado <rafael.hurtado@gmail.com>
2017-07-03 14:53:31 -0500

    Creando Activity Login

--- app/src/main/java/pe/edu/upc/quyawar/WelcomeActivity.java
+++ app/src/main/java/pe/edu/upc/quyawar/WelcomeActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -25,7 +26,10 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     private  void attemptWelcome(){
+        //TextView tv = (TextView) findViewById( R.id.tv_welcome);
+        //tv.setText("diste click");
 
+        //setContentView(R.layout.activity_login);
     }
 
 }
