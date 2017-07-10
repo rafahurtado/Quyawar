c37d66e29db00e135f59cc291e52fba36a6f589b
rafahurtado <rafael.hurtado@gmail.com>
2017-07-10 14:46:52 -0500

    Utimas actualizaciones

---
+++ app/src/main/java/pe/edu/upc/quyawar/WelcomeActivity.java
@@ -0,0 +1,35 @@
+package pe.edu.upc.quyawar;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+        Button btnIngresar = (Button) findViewById(R.id.btnIngresarWelcome);
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptWelcome();
+            }
+        });
+
+    }
+
+    private  void attemptWelcome(){
+        TextView tv = (TextView) findViewById( R.id.tv_welcome);
+        tv.setText(R.string.app_name);
+
+        //setContentView(R.layout.activity_login);
+    }
+
+}
