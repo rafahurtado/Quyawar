44c2c69e0d4f3e981c429f85634e2071d84535bd
rafahurtado <rafael.hurtado@gmail.com>
2017-07-23 14:11:12 -0500

    Creating models

---
+++ app/src/main/java/pe/edu/upc/quyawar/models/Locals.java
@@ -0,0 +1,57 @@
+package pe.edu.upc.quyawar.models;
+
+/**
+ * Created by Rafael on 23/07/2017.
+ */
+
+public class Locals {
+
+    private int id;
+    private String name;
+    private String lat;
+    private String lon;
+    private int pictureId;
+
+    public Locals() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public int getPictureId() {
+        return pictureId;
+    }
+
+    public void setPictureId(int pictureId) {
+        this.pictureId = pictureId;
+    }
+}
